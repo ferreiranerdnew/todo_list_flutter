f644252fc5ae724e84c2769ea82a9ab88f1c2d34
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-17 08:27:44 -0300

    Autocommit a cada 15 minutos

--- lib/wigets/todo_list_item.dart
+++ lib/wigets/todo_list_item.dart
@@ -61,25 +61,38 @@ class TodoListItem extends StatelessWidget {
             ],
           ),
         ),
-        actionExtentRatio: 0.20,
-        actionPane: const SlidableStrechActionPane(),
-        secondaryActions: [
-          IconSlideAction(
-            caption: 'Delete',
-            color: Colors.red,
-            icon: Icons.delete,
-            onTap: () {
-              onDelete(todo);
-            },
+
+        endActionPane: ActionPane(
+            // A motion is a widget used to control how the pane animates.
+            motion: const ScrollMotion(),
+
+            // A pane can dismiss the Slidable.
+            dismissible: DismissiblePane(onDismissed: () {}),
+
+            // All actions are defined in the children parameter.
+            children: const [
+              // A SlidableAction can have an icon and/or a label.
+              SlidableAction(
+                onPressed: null,
+                backgroundColor: Color(0xFFFE4A49),
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+              ),
+              SlidableAction(
+                onPressed: null,
+                backgroundColor: Color(0xFF21B7CA),
+                foregroundColor: Colors.white,
+                icon: Icons.share,
+                label: 'Share',
+              ),
+            ],
           ),
-          IconSlideAction(
-            caption: 'Save',
-            color: Colors.blue,
-            icon: Icons.save,
-            onTap: () {              
-            },
-          ),
-        ],
+
+
+
+
+
       ),
     );
   }
